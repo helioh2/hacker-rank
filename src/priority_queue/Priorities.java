2dc6f8a197c45745b6e266112ae1bf4f6b3f65c0
Helio <heliohenrique3@gmail.com>
2019-07-24 17:24:50 -0300

    iniciando problema da priority queue

---
+++ src/priority_queue/Priorities.java
@@ -0,0 +1,12 @@
+package priority_queue;
+
+import java.util.List;
+
+public class Priorities {
+
+	public List<Student> getStudents(List<String> events) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/priority_queue/Student.java
@@ -0,0 +1,10 @@
+package priority_queue;
+
+public class Student {
+
+	public char[] getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
